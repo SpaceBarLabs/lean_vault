8e3b8281b36c94bad22705fe0bff02ea4239559d
Zach Morek <zmorek@gmail.com>
2025-04-08 09:37:31 -0400

    cmd/cli/main.go

---
+++ cmd/cli/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spacebarlabs/lean_vault/pkg/commands"
+)
+
+const version = "0.1.0"
+
+func main() {
+	if len(os.Args) < 2 {
+		printUsage()
+		os.Exit(1)
+	}
+
+	cmd := os.Args[1]
+	args := os.Args[2:]
+
+	var err error
+	switch cmd {
+	case "init":
+		if len(args) != 0 {
+			fmt.Fprintf(os.Stderr, "Usage: %s init\n", os.Args[0])
+			os.Exit(1)
+		}
+		err = commands.Init()
+	case "add":
+		if len(args) != 1 {
+			fmt.Fprintf(os.Stderr, "Usage: %s add <key-name>\n", os.Args[0])
+			os.Exit(1)
+		}
+		// TODO: Implement add command
+		fmt.Println("Add command not implemented yet")
+	case "get":
+		if len(args) != 1 {
+			fmt.Fprintf(os.Stderr, "Usage: %s get <key-name>\n", os.Args[0])
+			os.Exit(1)
+		}
+		// TODO: Implement get command
+		fmt.Println("Get command not implemented yet")
+	case "list":
+		if len(args) != 0 {
+			fmt.Fprintf(os.Stderr, "Usage: %s list\n", os.Args[0])
+			os.Exit(1)
+		}
+		err = commands.List()
+	case "remove":
+		if len(args) != 1 {
+			fmt.Fprintf(os.Stderr, "Usage: %s remove <key-name>\n", os.Args[0])
+			os.Exit(1)
+		}
+		// TODO: Implement remove command
+		fmt.Println("Remove command not implemented yet")
+	case "rotate":
+		if len(args) != 1 {
+			fmt.Fprintf(os.Stderr, "Usage: %s rotate <key-name>\n", os.Args[0])
+			os.Exit(1)
+		}
+		// TODO: Implement rotate command
+		fmt.Println("Rotate command not implemented yet")
+	case "usage":
+		if len(args) != 0 {
+			fmt.Fprintf(os.Stderr, "Usage: %s usage\n", os.Args[0])
+			os.Exit(1)
+		}
+		// TODO: Implement usage command
+		fmt.Println("Usage command not implemented yet")
+	case "version":
+		fmt.Printf("lean_vault version %s\n", version)
+	default:
+		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", cmd)
+		printUsage()
+		os.Exit(1)
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func printUsage() {
+	fmt.Fprintf(os.Stderr, `Usage: %s <command> [arguments]
+
+Commands:
+  init                Initialize the vault
+  add <key-name>      Add a new OpenRouter API key
+  get <key-name>      Retrieve a stored key
+  list               List all stored keys
+  remove <key-name>   Remove and revoke a key
+  rotate <key-name>   Rotate a key (create new + revoke old)
+  usage              Display usage information for all keys
+  version            Show version information
+
+For detailed usage instructions, see: https://github.com/spacebarlabs/lean_vault
+`, os.Args[0])
+}
